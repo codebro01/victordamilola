9a27c2caba3419718d4c81a1586078f5531bc78a
codebro <samsondamilola.99@gmail.com>
2025-06-05 04:08:54 +0100

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: 'https://victordamilola.vercel.app', // your site's URL
+  generateRobotsTxt: true, // (optional) generate robots.txt file
+  // You can add more options here if you want to exclude pages, etc.
+}
